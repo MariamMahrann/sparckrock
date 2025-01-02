66873aa859345f09e29cd16cb243840128f9d584
MairamMahrann <mariam.mmahran@gmail.com>
2025-01-02 13:52:27 +0300

    fix the website conflict error to be ignored

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 
 Cypress.Commands.add("login", (email, password) =>{
-    cy.visit('/')
+    cy.visit('/',{ failOnStatusCode: false })
     cy.get('#username').type(email)
     cy.get('#password').type(password)
     cy.get('#submit').click()
